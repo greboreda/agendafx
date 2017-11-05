98519293f677b8c52d8a521af3190dc965606b15
Guillermo Reboreda <greboreda@idealista.com>
2017-11-05 18:58:04 +0100

    adding app menu component

--- src/main/java/greboreda/agendafx/controllers/components/menu/AppMenu.java
+++ src/main/java/greboreda/agendafx/controllers/components/menu/AppMenu.java
@@ -4,6 +4,8 @@ import greboreda.agendafx.controllers.ViewLoader;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 
@@ -13,7 +15,9 @@ import java.util.ResourceBundle;
 public class AppMenu extends MenuBar implements Initializable {
 
 	@FXML
-	MenuItem quit;
+	private MenuItem quit;
+	@FXML
+	private MenuItem about;
 
 	public AppMenu() {
 		ViewLoader.load(this);
@@ -22,5 +26,11 @@ public class AppMenu extends MenuBar implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		quit.setOnAction(a -> Platform.exit());
+		about.setOnAction(a -> {
+			final Alert info = new Alert(AlertType.INFORMATION, resources.getString("app.about"));
+			info.setTitle(resources.getString("generic.about"));
+			info.setHeaderText(resources.getString("generic.dialog.info"));
+			info.showAndWait();
+		});
 	}
 }
